4d5827fcd2d3204c03bb41e0f9d3bfd15e90712c
GMBrandao <gabrielmartbrandao@gmail.com>
2024-02-28 21:19:14 -0300

    Initial commit

---
+++ bin/hello_world_flutter.dart
@@ -0,0 +1,5 @@
+import 'package:hello_world_flutter/hello_world_flutter.dart' as hello_world_flutter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hello_world_flutter.calculate()}!');
+}
---
+++ test/hello_world_flutter_test.dart
@@ -0,0 +1,8 @@
+import 'package:hello_world_flutter/hello_world_flutter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
